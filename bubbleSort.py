023ca689c367d1b42170cb7866608584314904f5
belk1ng <dmitriy.belkin.2001@mail.ru>
2020-12-16 11:08:10 +0300

    add bubble sort

---
+++ bubbleSort.py
@@ -0,0 +1,6 @@
+def bubble_Sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
